7e797351e87b20cce25718c4b2790de97f940912
Irmuun <rokahakor@gmail.com>
2022-05-28 04:09:21 -0700

    deploy webwalker update

--- paistisuite/paistisuite.gradle.kts
+++ paistisuite/paistisuite.gradle.kts
@@ -1,4 +1,4 @@
-version = "3.7.6"
+version = "3.8.0"
 
 project.extra["PluginName"] = "PaistiSuite"
 project.extra["PluginDescription"] = "Scripting framework by Paisti. Required by all other Paisti Plugins to work!"
